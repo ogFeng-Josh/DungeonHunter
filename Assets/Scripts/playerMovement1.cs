9449ee0770c30dce92d6334b9b337f48d58088ea
Josh <jmyphotographybooking@gmail.com>
2020-09-19 00:35:23 -0700

    reupload

---
+++ Assets/Scripts/playerMovement1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement1 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public GameObject bullet;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    void Update()
+    {
+
+        BounceMovement();
+        Shoot();
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);   
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject.Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+
+    void BounceMovement()
+    {
+        float distance = (this.transform.position - Camera.main.transform.position).z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x;
+        float topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).y;
+        float bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distance)).y;
+
+
+        Vector3 playerSize = GetComponent<Renderer>().bounds.size;
+
+        this.transform.position = new Vector3(
+        Mathf.Clamp(this.transform.position.x, leftBorder + playerSize.x / 2, rightBorder - playerSize.x / 2),
+        Mathf.Clamp(this.transform.position.y, topBorder + playerSize.x / 2, bottomBorder - playerSize.x / 2),
+        this.transform.position.x
+        );
+
+
+    }
+
+}
